80b91b873b2374205dca53dfa9f4236c91e06b70
Jackoberto <jackoberto01@gmail.com>
2021-09-14 16:09:37 +0200

    Adds GAS and implements some abilities

--- Source/Playground/Private/MyCharacter.cpp
+++ Source/Playground/Private/MyCharacter.cpp
@@ -2,8 +2,33 @@
 
 
 #include "MyCharacter.h"
-
+#include "AbilitySystemComponent.h"
 
 //----------------------------------------------------------------------------------------------------------------------
 // 
 //----------------------------------------------------------------------------------------------------------------------
+AMyCharacter::AMyCharacter()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystem");
+}
+
+UAbilitySystemComponent* AMyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AMyCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	}
+}
+
+void AMyCharacter::GrantAbility(TSubclassOf<UGameplayAbility> GameplayAbility, int Level)
+{
+	if (HasAuthority() && GameplayAbility)
+ 		AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(GameplayAbility, Level));
+}
--- Source/Playground/Public/MyCharacter.h
+++ Source/Playground/Public/MyCharacter.h
@@ -3,37 +3,42 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/Character.h"
 #include "MyCharacter.generated.h"
-
 
 //----------------------------------------------------------------------------------------------------------------------
 // FORWARD DECLARATIONS
 //----------------------------------------------------------------------------------------------------------------------
 
-
+class UAbilitySystemComponent;
+class UGameplayAbility;
 
 //----------------------------------------------------------------------------------------------------------------------
 // CLASS START
 //----------------------------------------------------------------------------------------------------------------------
 UCLASS(Blueprintable)
-class PLAYGROUND_API AMyCharacter : public ACharacter {
+class PLAYGROUND_API AMyCharacter : public ACharacter, public IAbilitySystemInterface {
 
 	GENERATED_BODY()
 
-	
+	AMyCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	virtual void PossessedBy(AController* NewController) override;
 
 public:
 
 //----------------------------------------------------------------------------------------------------------------------
 // PROPERTIES
 //----------------------------------------------------------------------------------------------------------------------
-
-
+	UPROPERTY(BlueprintReadOnly)
+	UAbilitySystemComponent* AbilitySystemComponent;
 	
 //----------------------------------------------------------------------------------------------------------------------
 // FUNCTIONS
 //----------------------------------------------------------------------------------------------------------------------
 
-
+	UFUNCTION(BlueprintCallable, Category="Abilities")
+	void GrantAbility(TSubclassOf<UGameplayAbility> GameplayAbility, int Level);
 };
